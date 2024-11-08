eba36954f856763088efc7ce90244c4c2b9206de
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2024-11-08 16:23:20 +0300

    fetched data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+const url = process.env.URL;
+const token = process.env.TOKEN;
+
+const fetchData = () => {
+    axios({
+        method: 'get',
+        url,
+        headers: { Authorization: `Bearer ${token}` },
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+app.get('/', async (req, res) => {
+    fetchData();
     res.send('Hello World!');
 });
 
